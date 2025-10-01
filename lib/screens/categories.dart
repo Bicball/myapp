8901891cffa02649be5299c3e771d7a6a5ce619a
bicmoura <bicmoura@gmail.com>
2025-10-01 11:00:39 +0000

    MealDetailsScreen

---
+++ lib/screens/categories.dart
@@ -0,0 +1,46 @@
+import 'package:myapp/widgets/category_grid_item.dart';
+import 'package:flutter/material.dart';
+import 'package:myapp/data/dummy_data.dart';
+import 'package:myapp/screens/meals.dart';
+import 'package:myapp/models/category.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(title: category.title , meals: filteredMeals),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pick your category')),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          //availableCategories.map((e) => CategoryGridItem(category: category)).toList()
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+              ),
+        ],
+      ),
+    );
+  }
+}
